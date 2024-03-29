de75466df126a0f6837dccfdf8593ccc269817cb
Dimitri Brukakis <dimitri@brukakis.net>
2024-03-29 09:45:28 +0100

    Initial Commit

---
+++ AudioFoundationSample/AudioFoundationSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudioFoundationSampleApp.swift
+//  AudioFoundationSample
+//
+//  Created by Dimitri Brukakis on 29.03.24.
+//
+
+import SwiftUI
+
+@main
+struct AudioFoundationSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
